cc68133f10f5ffc150b445c2f57f90bdc6029d55
amigin <amigin@gmail.com>
2023-12-19 23:56:56 +0200

    Fixed settings

--- src/settings_model.rs
+++ src/settings_model.rs
@@ -2,46 +2,17 @@ use serde::{Deserialize, Serialize};
 service_sdk::macros::use_settings!();
 
 #[derive(
-    my_settings_reader::SettingsModel, SdkSettingsTraits, Serialize, Deserialize, Debug, Clone,
+    my_settings_reader::SettingsModel,
+    AutoGenerateSettingsTraits,
+    SdkSettingsTraits,
+    Serialize,
+    Deserialize,
+    Debug,
+    Clone,
 )]
 pub struct SettingsModel {
-    pub sb_connection: String,
+    pub my_sb_tcp_host_port: String,
     pub seq_conn_string: String,
-    pub no_sql_reader: String,
+    pub my_no_sql_tcp_reader: String,
     pub my_telemetry: String,
-}
-
-#[async_trait::async_trait]
-impl MyNoSqlTcpConnectionSettings for SettingsReader {
-    async fn get_host_port(&self) -> String {
-        let read_access = self.settings.read().await;
-
-        read_access.no_sql_reader.clone()
-    }
-}
-
-#[async_trait::async_trait]
-
-impl MyServiceBusSettings for SettingsReader {
-    async fn get_host_port(&self) -> String {
-        let read_access = self.settings.read().await;
-
-        read_access.sb_connection.clone()
-    }
-}
-
-#[async_trait::async_trait]
-impl service_sdk::my_telemetry::my_telemetry_writer::MyTelemetrySettings for SettingsReader {
-    async fn get_telemetry_url(&self) -> String {
-        let read_access = self.settings.read().await;
-        read_access.my_telemetry.clone()
-    }
-}
-
-#[async_trait::async_trait]
-impl service_sdk::my_logger::my_seq_logger::SeqSettings for SettingsReader {
-    async fn get_conn_string(&self) -> String {
-        let read_access = self.settings.read().await;
-        read_access.seq_conn_string.clone()
-    }
 }
