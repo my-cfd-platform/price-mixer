eb66ab6af3d1d3e6184ccd4128a92222d607ab64
amigin <amigin@gmail.com>
2024-05-09 22:03:50 +0300

    Made Miliseconds to contract

--- src/background/mappers.rs
+++ src/background/mappers.rs
@@ -5,7 +5,8 @@ use crate::app::ToPublishItem;
 pub fn map_bid_ask_to_sb_model(model: ToPublishItem) -> BidAskSbModel {
     return BidAskSbModel {
         id: model.bid_ask.instrument_id,
-        date_time_unix_milis: model.bid_ask.date_time.get_date_time().unix_microseconds as u64,
+        date_time_unix_milis: model.bid_ask.date_time.get_date_time().unix_microseconds as u64
+            / 1000,
         bid: model.bid_ask.bid,
         ask: model.bid_ask.ask,
         base: model.instrument.base.to_string(),
@@ -21,7 +22,8 @@ pub fn map_bid_ask_to_sb_model_with_markup(
     if markup_bid == 0 && markup_ask == 0 {
         return BidAskSbModel {
             id: model.bid_ask.instrument_id,
-            date_time_unix_milis: model.bid_ask.date_time.get_date_time().unix_microseconds as u64,
+            date_time_unix_milis: model.bid_ask.date_time.get_date_time().unix_microseconds as u64
+                / 1000,
             bid: model.bid_ask.bid,
             ask: model.bid_ask.ask,
             base: model.instrument.base.to_string(),
@@ -33,7 +35,8 @@ pub fn map_bid_ask_to_sb_model_with_markup(
 
     return BidAskSbModel {
         id: model.bid_ask.instrument_id,
-        date_time_unix_milis: model.bid_ask.date_time.get_date_time().unix_microseconds as u64,
+        date_time_unix_milis: model.bid_ask.date_time.get_date_time().unix_microseconds as u64
+            / 1000,
         bid: model.bid_ask.bid + multiplier * markup_bid as f64,
         ask: model.bid_ask.ask + multiplier * markup_ask as f64,
         base: model.instrument.base.to_string(),
